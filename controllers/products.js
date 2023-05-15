518748d64e23433fab803c37e122086ee4d284fe
AnaLinsDev <analinsdev@gmail.com>
2023-05-15 18:04:07 -0300

    add numeric filters

--- controllers/products.js
+++ controllers/products.js
@@ -12,7 +12,7 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort, fields } = req.query;
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
   const queryObject = {};
 
   // Product fields
@@ -29,6 +29,36 @@ const getAllProducts = async (req, res) => {
     // $regex: name -> search to any name with the value of req.query.name
     // $options: 'i' ->  case sensitive
     // https://www.mongodb.com/docs/manual/reference/operator/query/regex/#mongodb-query-op.-regex
+  }
+  
+  // Numeric Filters
+  if (numericFilters) {
+    const operatorMap = {
+      ">": "$gt",
+      ">=": "$gte",
+      "=": "$eq",
+      "<": "$lt",
+      "<=": "$lte",
+    };
+
+    const regEx = /\b(<|>|>=|=|<|<=)\b/g;
+
+    let filters = numericFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`
+    );
+
+    // filters === price-$gt-90
+
+    const options = ["price", "rating"];
+
+    // Validate that the field on "numericFilters" were the valid values of "options"
+    filters = filters.split(",").forEach((item) => {
+      const [field, operator, value] = item.split("-");
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) };
+      }
+    });
   }
 
   // ________________________________
@@ -54,15 +84,24 @@ const getAllProducts = async (req, res) => {
 
   // ________________________________
 
+  // Pagination
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
+  // ________________________________
+
   /*
      Other options:
      - .limit(10) -> limit the quantity to 10
      - .skip(5) -> skip first 5 items
   */
 
-  const products = await result.limit(5);
+  const products = await result;
 
-  res.status(200).json({ products, quant: products.length });
+  res.status(200).json({ quant: products.length, products });
 };
 
 module.exports = {
